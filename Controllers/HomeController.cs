44d85358672a1afb73c96e81c241ace37efd9d76
Starovoytov Nikita <nikstarall@gmail.com>
2020-02-11 22:19:44 +0700

    first_boop

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestShitAgain
+{
+    public class HomeController : Controller
+    {
+
+        kenguEntities dataBase = new kenguEntities();
+
+        public ActionResult Index()
+        {
+
+            IEnumerable<goods> good = dataBase.goods;
+
+            ViewBag.Goods = good;
+
+            return View();
+        }
+
+    }
+}
